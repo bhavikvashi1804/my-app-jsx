1110c610b563f963598a873fe493d78452ba2027
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 17:51:42 +0530

    Challenge 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <div>
-     <h1>Hello World</h1>
-     <p>This is a paragraph</p>
+     <h1>Programming Languages</h1>
+     <ul>
+       <li>C</li>
+       <li>C++</li>
+       <li>JAVA</li>
+     </ul> 
   </div>,
   document.getElementById('root')
 );
-//render will take only single html element to render in first argument
-//2nd argument specify where to render
