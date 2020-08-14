9af9682ddb94fac4b8f133b62152b6562144edae
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 19:20:05 +0530

    Inline CSS Style 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import ReactDOM from "react-dom";
 import './App.css';
 
 
+const styleForH1={
+  color:"yellow",
+  fontSize:"20px",
+  border:"1px solid green"
+};
+
+styleForH1.color="cyan";
 
 ReactDOM.render(
-  <h1 style={{color:"red"}}>Hello World</h1>,
+  <h1 style={styleForH1}>Hello World</h1>,
   document.getElementById("root")
 );
 
 
 //here style will be written as JS object {color:'red'}
 //to indicate that this is JS we have to specify that by using {} 
+//here key must be camle case
