401f274452c62e65739f17bc5e5437c6f12c9e9c
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 17:54:52 +0530

    JSX access the variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name="Bhavik Vashi";
+
 ReactDOM.render(
   <div>
      <h1>Programming Languages</h1>
@@ -9,6 +11,7 @@ ReactDOM.render(
        <li>C++</li>
        <li>JAVA</li>
      </ul> 
+     <p>by {name}</p>
   </div>,
   document.getElementById('root')
 );
