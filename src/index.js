40e05943e4ae850acab1081a79e632ef528b5f8c
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 17:45:48 +0530

    ReactDOM.render first argument: html single element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <div>
+     <h1>Hello World</h1>
+     <p>This is a paragraph</p>
+  </div>,
   document.getElementById('root')
 );
+//render will take only single html element to render in first argument
+//2nd argument specify where to render
