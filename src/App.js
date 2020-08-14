19dacbdedc146c5493acd00106f66898efabda61
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 17:18:33 +0530

    first project run successfully by npm start and localhost:3000

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Bhavik Vashi
         </a>
       </header>
     </div>
