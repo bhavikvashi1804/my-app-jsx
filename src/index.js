d7c26500d25d4b09db4e91fefb09df1fca28e841
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-14 18:11:06 +0530

    JavaScript expression in JSX

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Bhavik Vashi";
 const fName="Bhavik";
 const lName="Vashi";
 
 ReactDOM.render(
   <div>
-    <h1>Programming Languages</h1>
-    <ul>
-      <li>C</li>
-      <li>C++</li>
-      <li>JAVA</li>
-    </ul>
-    <p>by {name}</p>
-    <p>My name is {fName} {lName}</p>
-    <p>ES6 Template Literal</p>
-    <p>Hello {`${fName} ${lName}`}</p>
-    <p>My roll no is {2 + 5}</p>
-    <p>Your lucky number is {Math.floor(Math.random()*10)} </p>
+    <p>Created by {fName+" "+lName}</p>
+    <p>Copyright @{(new Date().getFullYear())}</p>
   </div>,
   document.getElementById("root")
 );
