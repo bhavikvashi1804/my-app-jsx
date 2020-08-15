00d0f539b5498cfded2df46a6c7412fc71e6b575
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 08:07:54 +0530

    Initial Commit for React Component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,15 @@ import ReactDOM from "react-dom";
 
 import './App.css';
 
-
-const date=new Date();
-const currentTime=date.getHours();
-
-let greeting;
-
-const customStyle={
-  color:"",
-}
-
-if(currentTime<12){
-  greeting="Good Morning";
-  customStyle.color="yellow";
-}
-else if(currentTime<18){
-  greeting="Good Afternoon";
-  customStyle.color="cyan";
-}
-else{
-  greeting="Good Night";
-  customStyle.color="green";
-}
-
 ReactDOM.render(
-<h1 className="heading" style={customStyle}>{greeting}</h1>,
+  <div>
+    <h1>Mobile</h1>
+    <ul>
+      <li>Samsung Galaxy Note 10 lite</li>
+      <li>Samsung Galaxy S10 lite</li>
+      <li>OnePlus 7t</li>
+    </ul>
+  </div>,
   document.getElementById("root")
 );
 
-
-//here style will be written as JS object {color:'red'}
-//to indicate that this is JS we have to specify that by using {} 
-//here key must be camle case
