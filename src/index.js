96e98deb4e83fd7a185462313b2f96e54913618b
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 08:13:55 +0530

    Component in JS file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import './App.css';
+import "./App.css";
 
-ReactDOM.render(
-  <div>
-    <h1>Mobile</h1>
+function Heading() {
+  return <h1>Mobile</h1>;
+}
+
+function List() {
+  return (
     <ul>
       <li>Samsung Galaxy Note 10 lite</li>
       <li>Samsung Galaxy S10 lite</li>
       <li>OnePlus 7t</li>
     </ul>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
-
