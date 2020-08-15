0ab4b53eb584431ea795fd3fd273ae93c2cf37b1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 08:30:42 +0530

    Create Component into JSX and use it into React

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Samsung Galaxy Note 10 lite</li>
+      <li>Samsung Galaxy S10 lite</li>
+      <li>OnePlus 7t</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App.jsx"
 
 import "./App.css";
 
-function Heading() {
-  return <h1>Mobile</h1>;
-}
-
-function List() {
-  return (
-    <ul>
-      <li>Samsung Galaxy Note 10 lite</li>
-      <li>Samsung Galaxy S10 lite</li>
-      <li>OnePlus 7t</li>
-    </ul>
-  );
-}
-
 ReactDOM.render(
-  <div>
-    <Heading />
-    <List />
-  </div>,
+  <App />,
   document.getElementById("root")
 );
